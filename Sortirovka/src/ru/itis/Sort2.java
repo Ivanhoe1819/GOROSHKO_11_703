bbf588ee6fdbd4e0a29531dd4edb89e2b24bf5ac
Ivanhoe1819 <leto4499@gmail.com>
2017-11-01 23:14:16 +0300

    Sortirovochki

--- Sortirovka/src/ru/itis/Sort2.java
+++ Sortirovka/src/ru/itis/Sort2.java
@@ -4,29 +4,30 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Sort2 {
-
     public static void main(String[] args) {
         Scanner q = new Scanner(System.in);
-        Random r = new Random();
-        int n, k, g;
+        Random R = new Random();
+        int max, z, x, n;
         System.out.println("Entering N");
         n = q.nextInt();
         int[] a = new int[n];
         for (int i = 0; i < n; i++) {
-            a[i] = r.nextInt(100);
-        }
-        for (int i = 0; i < n; i++) {
+            a[i] = R.nextInt(10);
             System.out.print(a[i] + " ");
         }
-        System.out.println();
-        for (k = 0; k < n; k++) {
-            for (int i = k + 1; i < n; i++) {
-                if (a[k]>a[i]){
-                    g = a[i];
-                    a[i]= a[k];
-                    a[k]=g;
+        System.out.println(" ");
+        for (int i = 0; i < n; i++) {
+            max = a[i];
+            z = i;
+            for (int k = i + 1; k < n; k++) {
+                if (a[k] <= max) {
+                    max = a[k];
+                    z = k;
                 }
             }
+            x = a[z];
+            a[z] = a[i];
+            a[i] = x;
         }
         for (int i = 0; i < n; i++) {
             System.out.print(a[i] + " ");
